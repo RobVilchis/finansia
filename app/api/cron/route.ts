e174d6043678679b8f7290196aa4dc58e54c66b7
Rob Vilchis <roberto180702@gmail.com>
2026-03-20 21:32:35 -0600

    Explicitly disable caching

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -14,6 +14,7 @@ import { z } from "zod";
 export const maxDuration = 300;
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 // Define the schema for tips
 const tipSchema = z.object({
